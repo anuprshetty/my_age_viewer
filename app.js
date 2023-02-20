da12ef8d2c5ef1f4e6ecc0b98f9257dfafbc364a
anuprshetty <anuprshetty@gmail.com>
2023-02-20 16:10:31 +0530

    bug fix

--- app.js
+++ app.js
@@ -9,47 +9,47 @@ var ageYear = document.getElementById("ageYear");
 var ageMonth = document.getElementById("ageMonth");
 var ageDays = document.getElementById("ageDays");
 var ageHours = document.getElementById("ageHours");
+var ageMinutes = document.getElementById("ageMinutes");
 var ageSeconds = document.getElementById("ageSeconds");
-var ageMiliSeconds = document.getElementById("ageMiliSeconds");
 
 setInterval(function () {
+  const today = new Date();
+  const birthDate = new Date(datePicker.value);
+
   var options = { year: "numeric", month: "long", day: "numeric" };
-  var selectedDate = new Date(datePicker.value);
-  var DOB = selectedDate.toLocaleDateString("en-US", options);
-
+  var DOB = birthDate.toLocaleDateString("en-US", options);
   choseDate.innerHTML = "DOB : " + " " + DOB;
-
-  var miliSeconds_Btw_DOB = Date.parse(DOB);
-  var miliSecond_Btw_Now = Date.now();
-
-  var age_in_MiliSeconds = miliSecond_Btw_Now - miliSeconds_Btw_DOB;
-
-  // console.log(age_in_MiliSeconds);
-
-  var miliSeconds = age_in_MiliSeconds;
-  var second = 1000;
-  var minute = second * 60;
-  var hour = minute * 60;
-  var day = hour * 24;
-  var month = day * 30;
-  var year = day * 365;
 
   // NOW START THE CALCULATION
 
-  var years = Math.round(miliSeconds / year);
-  var months = years * 12;
-  var days = years * 365;
-  var hours = Math.round(miliSeconds / hour);
-  var seconds = Math.round(miliSeconds / second);
+  let year = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+  if (
+    monthDiff < 0 ||
+    (monthDiff === 0 && today.getDate() < birthDate.getDate())
+  ) {
+    year--;
+  }
+  const month = (today.getMonth() + 12 - birthDate.getMonth()) % 12;
+  const dayDiff = today.getDate() - birthDate.getDate();
+  const day = dayDiff < 0 ? dayDiff + 30 : dayDiff;
+
+  const diffMs = today.getTime() - birthDate.getTime();
+  const diffSec = Math.floor(diffMs / 1000);
+  const sec = diffSec % 60;
+  const diffMin = Math.floor(diffSec / 60);
+  const min = diffMin % 60;
+  const diffHrs = Math.floor(diffMin / 60);
+  const hrs = diffHrs % 24;
 
   // Now it is time to print values in boxes
 
-  ageYear.innerHTML = years;
-  ageMonth.innerHTML = months;
-  ageDays.innerHTML = days;
-  ageHours.innerHTML = hours;
-  ageSeconds.innerHTML = seconds;
-  ageMiliSeconds.innerHTML = miliSeconds;
+  ageYear.innerHTML = year;
+  ageMonth.innerHTML = month;
+  ageDays.innerHTML = day;
+  ageHours.innerHTML = hrs;
+  ageMinutes.innerHTML = min;
+  ageSeconds.innerHTML = sec;
 
   document.querySelector(".age-calc").classList.add("expand");
 });
